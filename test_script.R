0091fada79bfd81acab69dd51856a5c32c3762db
emmagemal <emmagemal@gmail.com>
2021-02-22 16:30:49 +0000

    Emma added C gain efficiency

--- test_script.R
+++ test_script.R
@@ -5,6 +5,7 @@
 ## Library ----
 library(tidyverse)
 
+## Temperature Response Curves ----
 fulldata <- read.csv("np_dr_averages.csv", header = TRUE)
 str(fulldata)
 
@@ -12,23 +13,49 @@ np_only <- fulldata[fulldata$type == "NP", ]
 dr_only <- fulldata[fulldata$type == "DR", ]
               
 
-# initial plots
+# initial temperature curve plots
 (all_plot <- ggplot(fulldata, aes(x = temp, y = avgDW)) +
                 geom_point(aes(color = treatment_type)))
 
 (np_plot <- ggplot(np_only, aes(x = temp, y = avgDW)) +
-                geom_line(aes(color = treatment_type)))
+                geom_line(aes(color = treatment_type)) +
+                geom_point(aes(color = treatment_type)))
 
 (dr_plot <- ggplot(dr_only, aes(x = temp, y = avgDW)) +
-                geom_line(aes(color = treatment_type)))
+                geom_line(aes(color = treatment_type))+
+                geom_point(aes(color = treatment_type)))
 # respiration has acclimated to remain exactly the same as the control!! 
 
 
 # using chlorophyll data instead
 (np_chl_plot <- ggplot(np_only, aes(x = temp, y = avgChl)) +
-    geom_line(aes(color = treatment_type)))
+    geom_line(aes(color = treatment_type)) +
+    geom_point(aes(color = treatment_type)))
 
 (dr_chl_plot <- ggplot(dr_only, aes(x = temp, y = avgChl)) +
-    geom_line(aes(color = treatment_type)))
+    geom_line(aes(color = treatment_type)) +
+    geom_point(aes(color = treatment_type)))
 
 # basically the same thing, which is good! 
+
+
+## Carbon Gain Efficiency ----
+cgain <- read.csv("c_gain_efficiency.csv")
+
+dw_cgain <- cgain %>% 
+              dplyr::select(c("temp", "DRperc_DW", "NPperc_DW", "treatment_type"))
+
+control_dw <- dw_cgain[dw_cgain$treatment_type == "control", ]
+treatment_dw <- dw_cgain[dw_cgain$treatment_type == "treatment", ]
+
+control_dw_long <- control_dw %>% 
+                      gather(DWtype, percent, 2:3)
+treatment_dw_long <- treatment_dw %>% 
+                      gather(DWtype, percent, 2:3)
+
+# making a stacked plot for control 
+(stacked_control <- ggplot(control_dw_long, aes(x = temp, y = percent, fill = DWtype)) +
+                      geom_bar(position = "fill", stat = "identity"))
+
+(stacked_treatment <- ggplot(treatment_dw_long, aes(x = temp, y = percent, fill = DWtype)) +
+                        geom_bar(position = "fill", stat = "identity"))
