9e714b1a8d3962d4da060f6a4c4907514dbcc1ee
emmagemal <emmagemal@gmail.com>
2021-03-22 13:05:56 +0000

    Emma added the data and created more plots

--- test_script.R
+++ test_script.R
@@ -1,4 +1,4 @@
-# Initial Data Manipulation and Test Plots 
+# Initial Data Manipulation and Test Plots (PLOT SCRIPT ONLY, NOT STATS)
 # Emma Gemal, s1758915@sms.ed.ac.uk
 # University of Edinburgh 
 
@@ -6,16 +6,26 @@
 library(tidyverse)
 
 ## Temperature Response Curves ----
-fulldata <- read.csv("np_dr_averages.csv", header = TRUE)
+fulldata <- read.csv("Data/np_dr_averages.csv", header = TRUE)
 str(fulldata)
 
-np_only <- fulldata[fulldata$type == "NP", ]
-dr_only <- fulldata[fulldata$type == "DR", ]
-              
+np_only <- fulldata[fulldata$type == "NP", ]  # don't think I need
+dr_only <- fulldata[fulldata$type == "DR", ]  # don't think I need 
+
 
 # initial temperature curve plots
 (all_plot <- ggplot(fulldata, aes(x = temp, y = avgDW)) +
-                geom_point(aes(color = treatment_type)))
+                geom_point(aes(color = treatment_type, shape = type), 
+                           size = 2.5, alpha = 0.85))
+
+(facet_plot <- ggplot(fulldata, aes(x = temp, y = avgDW)) +
+                  geom_point(aes(shape = treatment_type, color = treatment_type),
+                             size = 2) +
+                  geom_line(aes(color = treatment_type)) +
+                  facet_wrap(vars(type), nrow = 1) +
+                  geom_errorbar(aes(ymin = avgDW-se_DW, ymax = avgDW+se_DW,
+                                    color = treatment_type),
+                                width = 0.3))  # USE THIS ONE I THINK! 
 
 (np_plot <- ggplot(np_only, aes(x = temp, y = avgDW)) +
                 geom_line(aes(color = treatment_type)) +
@@ -29,33 +39,107 @@ dr_only <- fulldata[fulldata$type == "DR", ]
 
 # using chlorophyll data instead
 (np_chl_plot <- ggplot(np_only, aes(x = temp, y = avgChl)) +
-    geom_line(aes(color = treatment_type)) +
-    geom_point(aes(color = treatment_type)))
+                  geom_line(aes(color = treatment_type)) +
+                  geom_point(aes(color = treatment_type)))
 
 (dr_chl_plot <- ggplot(dr_only, aes(x = temp, y = avgChl)) +
-    geom_line(aes(color = treatment_type)) +
-    geom_point(aes(color = treatment_type)))
+                  geom_line(aes(color = treatment_type)) +
+                  geom_point(aes(color = treatment_type)))
 
-# basically the same thing, which is good! 
+# basically the same relationship, which is good! Slightly different values of course 
 
 
 ## Carbon Gain Efficiency ----
-cgain <- read.csv("c_gain_efficiency.csv")
+cgain <- read.csv("Data/c_gain_efficiency.csv")  
 
-dw_cgain <- cgain %>% 
-              dplyr::select(c("temp", "DRperc_DW", "NPperc_DW", "treatment_type"))
+# separating the control and treatment data 
+control_dw <- cgain[cgain$treatment_type == "control", ]
+treatment_dw <- cgain[cgain$treatment_type == "treatment", ]
 
-control_dw <- dw_cgain[dw_cgain$treatment_type == "control", ]
-treatment_dw <- dw_cgain[dw_cgain$treatment_type == "treatment", ]
+# putting dark respiration and net photosynthesis values into a long column 
+control_perc_long <- control_dw %>% 
+                        gather(type, percent, 5:6) %>% 
+                        mutate(type = case_when(type == "DRperc_DW" ~ "DR",
+                                                type == "NPperc_DW" ~ "NP"))
+control_ratio_long <- control_dw %>% 
+                        gather(type, ratio, 3:4) %>% 
+                        mutate(type = case_when(type == "DRratio_DW" ~ "DR",
+                                                type == "NPratio_DW" ~ "NP"))
+control_long_combo <- full_join(control_perc_long, control_ratio_long) %>% 
+                        dplyr::select(c("temp", "GP_DW", "treatment_type",
+                                        "type", "percent", "ratio"))
+                      
+treatment_perc_long <- treatment_dw %>% 
+                          gather(type, percent, 5:6) %>% 
+                          mutate(type = case_when(type == "DRperc_DW" ~ "DR",
+                                                  type == "NPperc_DW" ~ "NP"))
+treatment_ratio_long <- treatment_dw %>% 
+                          gather(type, ratio, 3:4) %>% 
+                          mutate(type = case_when(type == "DRratio_DW" ~ "DR",
+                                                  type == "NPratio_DW" ~ "NP"))
+treatment_long_combo <- full_join(treatment_perc_long, treatment_ratio_long) %>% 
+                          dplyr::select(c("temp", "GP_DW", "treatment_type",
+                                          "type", "percent", "ratio"))
 
-control_dw_long <- control_dw %>% 
-                      gather(DWtype, percent, 2:3)
-treatment_dw_long <- treatment_dw %>% 
-                      gather(DWtype, percent, 2:3)
+# combining treatments for plotting
+cgain_combo <- full_join(treatment_long_combo, control_long_combo)
+                      
 
-# making a stacked plot for control 
-(stacked_control <- ggplot(control_dw_long, aes(x = temp, y = percent, fill = DWtype)) +
+# making stacked plots 
+(stacked_DWc <- ggplot(control_long_combo, aes(x = temp, y = percent, fill = type)) +
+                    geom_bar(position = "fill", stat = "identity"))
+# greatest proportion as DR/ smallest as NP at 20 degrees (similar to proportion at 15 though)
+
+(stacked_DWt <- ggplot(treatment_long_combo, aes(x = temp, y = percent, fill = type)) +
                       geom_bar(position = "fill", stat = "identity"))
+# greatest proportion as DR/smallest as NP at 25
 
-(stacked_treatment <- ggplot(treatment_dw_long, aes(x = temp, y = percent, fill = DWtype)) +
-                        geom_bar(position = "fill", stat = "identity"))
+(stacked_both <- ggplot(cgain_combo, aes(x = temp, y = percent, fill = type)) +
+                    geom_bar(position = "fill", stat = "identity") +
+                    facet_wrap(vars(treatment_type), nrow = 1))  # USE THIS I THINK!
+
+# change in ratio between respiration and (gross) photosynthesis 
+(dr_gp_plot <- ggplot(cgain, aes(x = temp, y = DRratio_DW)) +
+                  geom_point(aes(color = treatment_type)) +
+                  geom_line(aes(color = treatment_type, linetype = treatment_type)))
+# treatment appears to have acclimated, has lower ratios = has smaller R 
+ # or larger GP at higher temperatures 
+
+## could potentially use a beta distribution for a glm model, as it uses values BETWEEN 0 and 1
+
+
+## Acclimation Ratios ----
+ratios <- read.csv("Data/acclim_ratios.csv")
+
+(ratio_plot <- ggplot(ratios, aes(x = temp, y = DWc.t)) +
+                  geom_point(aes(color = type, shape = type), size = 2.5, alpha = 0.9) +
+                  geom_hline(yintercept = 1, linetype = "dotted"))
+
+# DR = 10-30 degrees the dark respiration ratios are nearly identical/ very similar
+  # could imply Q10 is the same for those temperatures, so >5 degrees 
+# closer to 1 would imply acclimation to the same rates as the control at optimal temp
+  # >1 would imply increased rates compared to the control 
+  # closer to 0 would imply less acclimation 
+# DR 2 and 5 degrees > 1, so higher respiration rates
+# NP 25 > 1, so higher net photosynthesis rates (>2x) and thus increased productivity 
+
+
+## Light Response Curves ----
+light <- read.csv("Data/light_responses.csv")
+str(light)
+summary(light)  # max control = 2.881, max treatment = 14.420 
+
+0.9*2.881  # 90% max control = 2.593
+0.9*14.420  # 90% max treatment = 12.978
+
+light <- light %>% 
+            pivot_longer(cols = 2:3,
+                         names_to = "treatment_type",
+                         names_prefix = "avgCO2_",
+                         values_to = "avgCO2")
+
+# plotting the light response curves
+(light_plots <- ggplot(light, aes(x = Lcuv, y = avgCO2)) +
+                  geom_point() +
+                  geom_line() +
+                  facet_wrap(vars(treatment_type)))
